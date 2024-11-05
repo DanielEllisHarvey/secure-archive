5cdca4d6533929475a6ca342de9e5a0dcd4644d8
danielellisharvey <danielellisharvey@outlook.com>
2024-11-05 16:57:46 +0000

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#? Any custom directory changes will not be reflected in this installer, and directories must be created manually
+# Create directories in user space
+mkdir ~/.config/secarch/
+mkdir ~/.secarch/
+mkdir ~/Documents/archived-items/
+mkdir ~/usr/share/secarch/
+
+mv config/config.json ~/.config/secarch/
+mv keygen/* usr/share/secarch/
+
+chmod +r usr/share/secarch/*
