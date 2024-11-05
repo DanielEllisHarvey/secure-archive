e3b717c59ca38f4a65bb2f789b53267b664e39a0
danielellisharvey <danielellisharvey@outlook.com>
2024-11-05 17:21:17 +0000

    Fixed minor errors, fully implemented file key generation

--- keygen/derive_new.py
+++ keygen/derive_new.py
@@ -23,21 +23,22 @@ file_id = b64encode(file_byte_id).decode()
 file_data = open(sys.argv[1], "rb").read()
 
 file_key_iv = urandom(32)
-file_key = hmac_sha512(master_key, (a ^ b for a, b in zip(file_byte_id, salt)))
+file_key = hmac_sha512(master_key, bytes(a ^ b for a, b in zip(file_byte_id, salt))).digest()
 file_key_enc = aes_gcm_encrypt(decryption_key, file_key_iv, file_key)
 
 with open(home_dir+config["working_dir"]+"index", "a") as index:
-    index.append(
-        sys.argv[1], # File name
-        hmac_sha512(master_key, bytes(sys.argv[1], "utf-8")+file_byte_id).hexdigest(), # File name + id integrity
-        hmac_sha512(master_key, file_data+file_byte_id).hexdigest(), # File data integrity
-        file_id, "\n"
+    index.write(
+        sys.argv[1]+" "+ # File name
+        hmac_sha512(master_key, bytes(sys.argv[1], "utf-8")+file_byte_id).hexdigest()+" "+ # File name + id integrity
+        hmac_sha512(master_key, file_data+file_byte_id).hexdigest()+" "+ # File data integrity
+        file_id+"\n"
     )
     index.close()
 
 with open(home_dir+config["working_dir"]+config["key_index_file"], "a") as index:
-    index.append(
-        file_id,
-        b64encode(file_key_enc).decode(), # Password-encrypted file key
+    index.write(
+        file_id+" "+
+        b64encode(file_key_enc).decode()+" "+ # Password-encrypted file key
         "\n"
     )
+    index.close()
