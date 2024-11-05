d6c1c01c1db836819c4ff861bf7e9170dbbd50e2
danielellisharvey <danielellisharvey@outlook.com>
2024-11-05 17:00:57 +0000

    Fixed file structures with `HOME` env variable

--- keygen/derive_new.py
+++ keygen/derive_new.py
@@ -4,14 +4,15 @@ import json
 import getpass
 import sys
 from primitives_wrapper import aes_gcm_decrypt, aes_gcm_encrypt, one_pbkdf, hmac_sha512
-from os import urandom
+from os import urandom, getenv
 from base64 import b64encode
 
-config = json.loads(open(".config/secarch/config.json", "r").read())
+home_dir = getenv("HOME")
+config = json.loads(open(home_dir+"/.config/secarch/config.json", "r").read())
 
-master_key = open(config.working_dir+config.master_key_file, "rb").read()
-salt = open(config.working_dir+config.salt_file, "rb").read()
-passwd_salt = open(config.working_dir+config.passwd_salt_file, "rb").read()
+master_key = open(home_dir+config["working_dir"]+config["master_key_file"], "rb").read()
+salt = open(home_dir+config["working_dir"]+config["salt_file"], "rb").read()
+passwd_salt = open(home_dir+config["working_dir"]+config["passwd_salt_file"], "rb").read()
 
 passwd = getpass.getpass("passwd: ")
 decryption_key = one_pbkdf(passwd, passwd_salt)
@@ -25,7 +26,7 @@ file_key_iv = urandom(32)
 file_key = hmac_sha512(master_key, (a ^ b for a, b in zip(file_byte_id, salt)))
 file_key_enc = aes_gcm_encrypt(decryption_key, file_key_iv, file_key)
 
-with open(config.working_dir+"index", "a") as index:
+with open(home_dir+config["working_dir"]+"index", "a") as index:
     index.append(
         sys.argv[1], # File name
         hmac_sha512(master_key, bytes(sys.argv[1], "utf-8")+file_byte_id).hexdigest(), # File name + id integrity
@@ -34,7 +35,7 @@ with open(config.working_dir+"index", "a") as index:
     )
     index.close()
 
-with open(config.working_dir+config.key_index_file, "a") as index:
+with open(home_dir+config["working_dir"]+config["key_index_file"], "a") as index:
     index.append(
         file_id,
         b64encode(file_key_enc).decode(), # Password-encrypted file key
