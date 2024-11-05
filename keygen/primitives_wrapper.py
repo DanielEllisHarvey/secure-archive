a948fc5e5e9fc688cb65536e2d7836867886b1e3
danielellisharvey <danielellisharvey@outlook.com>
2024-11-05 17:00:28 +0000

    Fixed kdf lengths, added openssl backend, and changed to pyca `hashes`

--- keygen/primitives_wrapper.py
+++ keygen/primitives_wrapper.py
@@ -1,33 +1,39 @@
 from cryptography.hazmat.primitives.kdf import pbkdf2, hkdf
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, modes, algorithms
+from cryptography.hazmat.backends import openssl
 import hmac
 import hashlib
 
 def aes_gcm_encrypt(key, iv, data):
     return Cipher(
         algorithms.AES256(key),
-        modes.GCM(iv, min_tag_length=128)
+        modes.GCM(iv, min_tag_length=128),
+        backend=openssl.backend
     ).encryptor().update(data)
 
 def aes_gcm_decrypt(key, iv, ciphertext):
     return Cipher(
         algorithms.AES256(key),
-        modes.GCM(iv, min_tag_length=128)
+        modes.GCM(iv, min_tag_length=128),
+        backend=openssl.backend
     ).decryptor().update(ciphertext)
 
 def one_hkdf(key, salt):
     return hkdf.HKDF(
-        algorithm=hashlib.sha256,
-        length=256,
-        salt=salt
+        algorithm=hashes.SHA3_256(),
+        length=32,
+        salt=salt,
+        backend=openssl.backend
     ).derive(key)
 
 def one_pbkdf(passwd, salt):
     return pbkdf2.PBKDF2HMAC(
-        algorithm=hashlib.sha3_256,
-        length=256,
+        algorithm=hashes.SHA3_256(),
+        length=32,
         salt=salt,
-        iterations=1000
+        iterations=1000,
+        backend=openssl.backend
     ).derive(bytes(passwd, "utf-8"))
     
 def hmac_sha512(key, data):
