aa53d0727c92732e66343d94cbe363b3789ad927
zenex458 <emajeed458@gmail.com>
2024-11-07 16:00:29 +0000

    install.sh: changed ~/usr/share to ~/.local/share, as it says in xdg spec

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 
 #? Any custom directory changes will not be reflected in this installer, and directories must be created manually
 # Create directories in user space
 mkdir -p ~/.config/secarch/
 mkdir -p ~/.secarch/
 mkdir -p ~/Documents/archived-items/
-mkdir -p ~/usr/share/secarch/
+mkdir -p ~/.local/share/secarch
 
 mv config/config.json ~/.config/secarch/
-mv keygen/* ~/usr/share/secarch/
+mv keygen/* ~/.local/share/secarch
 
-chmod +r ~/usr/share/secarch/*
+chmod +r ~/.local/share/secarch*
